d9eca5d1660533b5b4de2656c659d6a6a112652c
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-03-31 20:01:09 +0300

    Add nix specific tools

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for qmk";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+  in {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = with pkgs; [
+        qmk
+      ];
+    };
+  };
+}
