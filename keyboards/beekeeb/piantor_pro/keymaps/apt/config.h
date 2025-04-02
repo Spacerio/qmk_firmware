42f145940105c169a2e05e0070201fc5e662f0e8
Oiva Laitinen <oiva.laitinen2@gmail.com>
2025-04-02 18:38:03 +0300

    Enable home row mods

--- keyboards/beekeeb/piantor_pro/keymaps/apt/config.h
+++ keyboards/beekeeb/piantor_pro/keymaps/apt/config.h
@@ -1,2 +1,3 @@
 #define DEBOUCE 20
 #define COMBO_ONLY_FROM_LAYER 0
+#define CHORDAL_HOLD
--- keyboards/beekeeb/piantor_pro/keymaps/apt/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/apt/keymap.c
@@ -11,12 +11,33 @@ enum layers {
     MISC_LAYER,
 };
 
+// BASE_LAYER mod taps
+#define MT_S MT(MOD_LGUI, KC_S)
+#define MT_T MT(MOD_LCTL, KC_T)
+#define MT_H MT(MOD_LALT, KC_H)
+#define MT_N MT(MOD_LALT, KC_N)
+#define MT_E MT(MOD_LCTL, KC_E)
+#define MT_A MT(MOD_LGUI, KC_A)
+// NUM_LAYER mod taps
+#define MT_4 MT(MOD_LGUI, KC_4)
+#define MT_5 MT(MOD_LCTL, KC_5)
+#define MT_6 MT(MOD_LALT, KC_6)
+#define MT_DOWN MT(MOD_LALT, KC_DOWN)
+#define MT_UP MT(MOD_LCTL, KC_UP)
+#define MT_RIGHT MT(MOD_LGUI, KC_RIGHT)
+// SYM_LAYER mod taps
+#define MT_MINS MT(MOD_LGUI, KC_MINS)
+#define MT_ASTR MT(MOD_LCTL, KC_ASTR)
+#define MT_EQL MT(MOD_LALT, KC_EQL)
+#define MT_DLR MT(MOD_LALT, KC_DLR)
+#define MT_RPRN MT(MOD_LCTL, KC_RPRN)
+#define MT_LPRN MT(MOD_LGUI, KC_LPRN)
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE_LAYER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_W,    KC_G,    KC_D,    KC_F,    KC_B,                         KC_Q,    KC_L,    KC_U,    KC_O,   KC_Y,  KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ESC,    KC_R,    KC_S,    KC_T,    KC_H,    KC_K,                         KC_J,    KC_N,    KC_E,    KC_A,    KC_I, KC_SCLN,
+       KC_ESC,    KC_R,    MT_S,    MT_T,    MT_H,    KC_K,                         KC_J,    MT_N,    MT_E,    MT_A,    KC_I, KC_SCLN,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LGUI,    KC_X,    KC_C,    KC_M,    KC_P,    KC_V,                         KC_Z, KC_COMM,  KC_DOT, KC_QUOT, KC_SLSH, KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -29,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB, XXXXXXX,    KC_1,    KC_2,    KC_3, XXXXXXX,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_0,    KC_4,    KC_5,    KC_6, XXXXXXX,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,
+      KC_LCTL,    KC_0,    MT_4,    MT_5,    MT_6, XXXXXXX,                      KC_LEFT, MT_DOWN,   MT_UP,MT_RIGHT, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LGUI, XXXXXXX,    KC_7,    KC_8,    KC_9, XXXXXXX,                      KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX, KC_RALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -41,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       QK_BOOT, KC_CIRC, KC_PERC, KC_SLSH, KC_AMPR, XXXXXXX,                      XXXXXXX, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, KC_UNDS, KC_MINS, KC_ASTR,  KC_EQL, KC_TILD,                       KC_GRV,  KC_DLR, KC_LPRN, KC_RPRN, KC_EXLM, XXXXXXX,
+      XXXXXXX, KC_UNDS, MT_MINS, MT_ASTR,  MT_EQL, KC_TILD,                       KC_GRV,  MT_DLR, MT_LPRN, MT_RPRN, KC_EXLM, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, XXXXXXX, KC_PLUS, KC_BSLS, KC_PIPE, XXXXXXX,                      XXXXXXX, KC_HASH,   KC_AT, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
